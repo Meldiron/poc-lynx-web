c01cfce53bb171c975d7c04d470870ad99dbac20
Matej Bačo <matejbaco2000@gmail.com>
2025-03-11 18:20:43 +0100

    Fix build

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import { root } from '@lynx-js/react'
+import { root } from "@lynx-js/react";
 
-import { App } from './App.js'
+import { App } from "./App.jsx";
 
-root.render(<App />)
+root.render(
+  <App />,
+);
 
 if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
+  import.meta.webpackHot.accept();
 }
